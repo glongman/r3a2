4aab2d0cdf383c588992b44f6671d317da6bcde2
Geoffrey Longman <glongman@gmail.com>
2011-01-16 13:55:06 -0500

    remove commented out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   before_filter :authenticate_user!
-
-  # Example of using the responders - this will render html, xml or json depending on the request
-  # def show
-  #   respond_with @user
-  # end
 end
