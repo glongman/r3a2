f2c681dc598e89ac1c4f34be94d30e78453d9364
Geoffrey Longman <glongman@gmail.com>
2011-01-16 13:50:45 -0500

    fix test (devise) that did not work out of the box

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get redirected on index when not logged in  " do
     get :index
-    assert_response :success
+    assert_redirected_to new_user_session_url
   end
 
 end
