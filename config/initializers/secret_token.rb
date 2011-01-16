3cc4753e0439803414fae233dc8c4b336f375526
Geoffrey Longman <glongman@gmail.com>
2011-01-16 12:56:32 -0500

    initial commit after running john Duff's rails template: https://github.com/jduff/rails-templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R3api::Application.config.secret_token = '09aaf58a241e63041bc65b89528e0f40bd2ec220a2650a2d835aebb7ad1364278819a4c3036110c19947b5a739a3c16012e3d25a0c681fa64c0b635eed2b44be'
