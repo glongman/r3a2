f0e1b4e5d67bffea70b667ca281779266d33a595
Geoffrey Longman <glongman@gmail.com>
2011-01-16 14:01:34 -0500

    dont show auth tokens in the logs

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module R3a2
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :auth_token]
   end
 end
