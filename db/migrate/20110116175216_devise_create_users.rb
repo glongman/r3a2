0b5a1fbb7a63d0ac2be7d647553cf2921d03aa63
Geoffrey Longman <glongman@gmail.com>
2011-01-16 14:13:39 -0500

    cleanup & improve user migration (I promise not to do this again!)

--- db/migrate/20110116175216_devise_create_users.rb
+++ db/migrate/20110116175216_devise_create_users.rb
@@ -3,22 +3,18 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       t.database_authenticatable :null => false
       t.recoverable
-      t.rememberable
       t.trackable
-
-      # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
-
+      t.token_authenticatable
+      t.lockable :lock_strategy => :none, :unlock_strategy => :none
       t.string :login
-
+      t.string :role, :null => false, :default => 'normal'
+      t.string :name
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
+    add_index :users, :login,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
   end
 
   def self.down
