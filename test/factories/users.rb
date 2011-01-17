002599772d276cb9e5146f2dd3e2d3b818624522
Geoffrey Longman <glongman@gmail.com>
2011-01-17 00:57:23 -0500

    add factory for user with auth token, clean up to use blocks instead of a mix

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,22 +4,29 @@ Factory.define :user do |f|
   f.sequence(:email)      {|n| "user#{n}@example.com" }
   f.sequence(:login)      {|n| "user#{n}" }
   f.sequence(:name)       {|n| "user#{n}.titleize"}
-  f.password              "password"
-  f.password_confirmation "password"
+  f.password              {"password"}
+  f.password_confirmation {"password"}
 end
 
 Factory.define :admin_user, :parent => :user do |f|
-  f.role "admin"
+  f.role {"admin"}
 end
 
 Factory.define :api_user, :parent => :user do |f|
-  f.role "api"
+  f.role {"api"}
 end
 
 Factory.define :normal_user, :parent => :user do |f|
-  f.role "normal"
+  f.role {"normal"}
 end
 
 Factory.define :disabled_user, :parent => :user do |f|
-  f.role "disabled"
+  f.role {"disabled"}
 end
+
+Factory.define :user_with_auth_token, :parent => :user do |f|
+  f.after_create do |user_record|
+    user_record.ensure_authentication_token!
+  end
+end
+    
