980eefe6bf89e4a82ef4fbbf7483136f29a0ae76
Geoffrey Longman <glongman@gmail.com>
2011-01-21 00:26:05 -0500

    crud work on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,33 @@
 class UsersController < ApiBase
   filter_resource_access
   def index
-    respond_with(@users = User.with_permissions_to(:update))
+    respond_with(@users = User.with_permissions_to(:show))
   end
-
+  
   def show
-    respond_with(@user)
+    respond_with @user
   end
 
   def new
-    respond_with(@user)
+    respond_with( @user = User.new, :template => 'edit')
+  end
+  
+  def edit
+    respond_with @user
   end
 
   def create
-    @user.update_attributes(params[:user])
-    respond_with(@user)
+    @user.update_attributes params[:user]
+    respond_with @user
   end
 
   def update
+    @user.update_attributes params[:user]
+    respond_with @user
   end
 
   def delete
+    respond_with @user.destroy
   end
 
 end
