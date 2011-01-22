9de01c7ca785a73bb5d40a537775625e5e433169
Geoffrey Longman <glongman@gmail.com>
2011-01-22 16:18:00 -0500

    adding players, scores, scoresheets to authorization rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -5,15 +5,28 @@ authorization do
       to :manage_locks
       if_attribute :role => is_not {'admin'}
     end
+    has_permission on :players, :to => :manage
+    has_permission on :scores, :to => :manage
+    has_permission on :scoresheets, :to => :manage
   end
   role :api do
     has_permission_on :users do
       to :manage
       if_attribute :role => is_not_in {%w(admin api)}
     end
+    has_permission on :players, :to => :manage
+    has_permission on :scores, :to => :manage
+    has_permission on :scoresheets, :to => :manage
   end
   role :normal do
     includes :self_service
+    has_permission on :players, :to => :manage
+    has_permission on :scores, :to => :manage do
+      if_attribute :player => {:user => is {user.id}}
+    end
+    has_permission on :scoresheets, :to => [:index, :show, :update] do
+      if_attribute :player => {:user => is {user.id}}
+    end
   end
   role :guest do
   end
