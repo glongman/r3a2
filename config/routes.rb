92bc78370daba9d98585505d3514ac3c2868147d
Geoffrey Longman <glongman@gmail.com>
2011-01-22 16:16:06 -0500

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,20 @@ R3a2::Application.routes.draw do
       put :lock, :action => 'lock'
       delete :lock, :action => 'unlock'
     end
+    resources :players, :only =>[:index, :create], :controller => :players
   end
+  resources :players, :except => [:new, :edit] do
+    member do
+      get :lock, :action => 'show_lock'
+      put :lock, :action => 'lock'
+      delete :lock, :action => 'unlock'
+    end
+    resources :scores, :only => [:index, :create]
+    resources :scoresheets, :only => [:index]
+    member do
+      get :scoresheet, :controller => :scoresheets, :action => 'show'
+    end
+  end
+  resources :scores, :except => [:create, :new, :edit]
+  resources :scoresheets, :except => [:create, :new, :edit, :update]
 end
