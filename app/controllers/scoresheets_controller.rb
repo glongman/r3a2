61d1e21b35b9cbca890c2f8857d8784363980e2a
Geoffrey Longman <glongman@gmail.com>
2011-01-22 16:17:35 -0500

    Scoresheets controller - very basic

---
+++ app/controllers/scoresheets_controller.rb
@@ -0,0 +1,15 @@
+class ScoresheetsController < ApplicationController
+  #         scoresheets GET    /scoresheets(.:format)                    
+  #  player_scoresheets GET    /players/:player_id/scoresheets(.:format) 
+  def index
+  end
+
+  #          scoresheet GET    /scoresheets/:id(.:format)        
+  #   scoresheet_player GET    /players/:id/scoresheet(.:format)
+  def show
+    
+  end
+
+end
+
+
