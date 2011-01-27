380a598f4881d571a7865a29bee588071b212f71
Geoffrey Longman <glongman@gmail.com>
2011-01-27 01:03:20 -0500

    add tests for UsersHelper

--- test/unit/helpers/users_helper_test.rb
+++ test/unit/helpers/users_helper_test.rb
@@ -1,4 +1,86 @@
 require 'test_helper'
-
+require 'builder'
 class UsersHelperTest < ActionView::TestCase
+  def setup
+    @user = Factory :admin_user
+    @default_keys = UsersHelper::USER_API_DEFAULT_ATTRIBUTES.collect(&:to_s) << 'url'
+  end
+  
+  def test_user_json_includes_default_keys
+    hash = as_user_json
+    assert_not_nil hash
+    @default_keys.each do |k| 
+      assert hash.has_key?(k), "expected key #{k.inspect} but didn't get it"
+    end
+  end
+  
+  def test_user_json_has_url
+    hash = as_user_json
+    assert hash.has_key?('url'), "expected url but didn't get it"
+    assert_equal user_path(@user), hash['url']
+  end
+  
+  def test_new_user_has_no_url
+    hash = as_user_json Factory.build :normal_user
+    assert !hash.has_key?('url'), 'expected no url but got one'
+  end
+  
+  def test_user_json_with_block_still_has_url
+    hash = as_user_json do |json|
+      json.merge!(:foo => :moo)
+    end
+    assert hash.has_key?('url'), "expected url but didn't get it"
+    assert_equal user_path(@user), hash['url']
+    assert hash.has_key?(:foo), "expected :foo but didn't get it"
+    assert_equal :moo, hash[:foo]
+  end
+  
+  def test_user_xml_includes_default_keys
+    xml = user_xml
+    assert !xml.blank?
+    hash = Hash.from_xml(xml)['user']
+    @default_keys.each do |k| 
+      assert hash.has_key?(k), "expected key #{k.inspect} but didn't get it"
+    end
+  end
+  
+  def test_user_xml_has_url
+    xml = user_xml
+    hash = Hash.from_xml(xml)['user']
+    assert hash.has_key?('url'), "expected url but didn't get it"
+    assert_equal user_path(@user, :format => :xml), hash['url']
+  end
+  
+  def test_new_user_xml_has_no_url
+    xml = user_xml Factory.build :normal_user
+    hash = Hash.from_xml(xml)['user']
+    assert !hash.has_key?('url'), 'expected no url but got one'
+  end
+  
+  def test_user_xml_with_block_still_has_url
+    xml = user_xml do |xml|
+      xml.foo "moo"
+    end
+    hash = Hash.from_xml(xml)['user']
+    assert hash.has_key?('url'), "expected url but didn't get it"
+    assert_equal user_path(@user, :format => :xml), hash['url']
+    assert hash.has_key?('foo'), "expected :foo but didn't get it"
+    assert_equal 'moo', hash['foo']
+  end
+  
+  def test_lock_json
+    @user.lock_access!
+    time = @user.locked_at
+    hash = as_lock_json
+    assert_equal({'locked_at' => time.as_json, 'user' => user_path(@user)}, hash)
+  end
+  
+  def test_lock_xml
+    @user.lock_access!
+    time = @user.locked_at
+    xml = lock_xml Builder::XmlMarkup.new
+    hash = Hash.from_xml(xml)['lock']
+    assert_equal({'locked_at' => time.as_json, 'user' => user_path(@user, :format => :xml)}, hash)
+  end
+  
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,41 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   
-  def setup
-    @user = Factory :user_with_auth_token
-    @default_keys = User::API_DEFAULT_ATTRIBUTES.collect(&:to_s)
-  end
   
-  def test_as_json_with_no_options_returns_default_json
-    hash = @user.as_json
-    assert_not_nil hash
-    assert_has_default_keys hash, true
-  end
-  
-  def test_json_with_irrelavent_options_returns_default_json
-    hash = @user.as_json :action => 'index'
-    assert_not_nil hash
-    assert_has_default_keys hash, true
-  end
-  
-  def test_json_with_valid_options_respects_those_options
-    hash = @user.as_json :only => 'id'
-    assert_not_nil hash
-    assert_equal({'id' => @user.id}, hash)
-  end
   # Replace this with your real tests.
   test "the truth" do
     assert true
   end
   
-  protected
-  def assert_has_default_keys(hash, only_default_keys)
-    hash = hash.dup
-    @default_keys.each do |k| 
-      assert hash.has_key?(k), "expected key #{k.inspect} but didn't get it"
-      hash.delete(k)
-    end
-    assert(hash.empty?, "expected only default keys but also got: #{hash.inspect}") if only_default_keys
-    hash
-  end
+  
 end
